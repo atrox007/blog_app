809eb69bf8bdea199c665f1206c34cc417f589b5
atrox007 <ashutosh1kumar007@gmail.com>
2024-12-27 01:21:11 +0530

    added redux toolkit

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,10 +1,13 @@
-import { StrictMode } from 'react'
+import React from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import {store} from './redux/store.js' ; 
+import { Provider } from 'react-redux'; 
 
+// @ts-ignore
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
+  <Provider store={store}>
     <App />
-  </StrictMode>,
+  </Provider>,
 )
--- client/src/pages/SignIn.jsx
+++ client/src/pages/SignIn.jsx
@@ -1,11 +1,17 @@
 import { Alert, Button, Label, Spinner, TextInput } from 'flowbite-react';
 import React, { useState } from 'react'
 import { Link, useNavigate } from 'react-router-dom'; 
+import { useDispatch, useSelector } from 'react-redux'; 
+import {signInStart, signInSuccess, signInFailure} from '../redux/user/userSlice.js' ; 
+
 
 export default function SignIn() {
   const [formData, setFormData] = useState({});
-  const [errorMessage, setErrorMessage] = useState(null);
-  const [loading, setLoading] = useState(false);
+  // const [errorMessage, setErrorMessage] = useState(null);
+  // const [loading, setLoading] = useState(false);
+  // @ts-ignore
+  const { loading, error: errorMessage } = useSelector((state) => state.user);
+  const dispatch = useDispatch() ; 
   const navigate = useNavigate();
   const handleChange = (e) => {
     setFormData({ ...formData, [e.target.id]: e.target.value.trim() });
@@ -15,11 +21,12 @@ export default function SignIn() {
     // @ts-ignore
     if (!formData.email || !formData.password) {
       // @ts-ignore
-      return setErrorMessage('Please fill out all fields.');
+      return dispatch(signInFailure('Please fill all the fields')); 
     }
     try {
-      setLoading(true);
-      setErrorMessage(null);
+      // setLoading(true);
+      // setErrorMessage(null);
+      dispatch(signInStart()) ; 
       const res = await fetch('/api/auth/signin', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
@@ -27,15 +34,18 @@ export default function SignIn() {
       });
       const data = await res.json();
       if (data.success === false) {
-        return setErrorMessage(data.message);
+        //return setErrorMessage(data.message);
+        dispatch(signInFailure(data.message)) ; 
       }
-      setLoading(false);
+      // setLoading(false);
       if(res.ok) {
+        dispatch(signInSuccess(data)) ; 
         navigate('/');
       }
     } catch (error) {
-      setErrorMessage(error.message);
-      setLoading(false);
+      // setErrorMessage(error.message);
+      // setLoading(false);
+      dispatch(signInFailure(error.message)) ;
     }
   }; 
   return (
