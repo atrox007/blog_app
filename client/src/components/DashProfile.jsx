61bb187a6c02fbefc0c76ce87ad15637cbe4e06a
atrox007 <ashutosh1kumar007@gmail.com>
2024-12-27 22:05:12 +0530

    profile page ui created

--- client/src/components/DashProfile.jsx
+++ client/src/components/DashProfile.jsx
@@ -1,7 +1,51 @@
+import { Button, TextInput } from 'flowbite-react';
 import React from 'react'
+import { useEffect, useRef, useState } from 'react';
+import { useSelector } from 'react-redux';
 
 export default function DashProfile() {
+    // @ts-ignore
+    const { currentUser, error, loading } = useSelector((state) => state.user);
   return (
-    <div>DashProfile</div>
+    <div className='max-w-lg mx-auto p-3 w-full'>
+        <h1 className='my-7 text-center font-semibold text-3xl'>Profile</h1>
+        <form className='flex flex-col gap-4'>
+            <div className='relative w-32 h-32 self-center cursor-pointer shadow-md overflow-hidden rounded-full'>
+                <img src={currentUser.profilePicture} alt="" className={`rounded-full w-full h-full object-cover border-8 border-[lightgray]`}/>
+            </div>
+            <TextInput
+            type='text'
+            id='username'
+            placeholder='username'
+            defaultValue={currentUser.username}
+            //onChange={handleChange}
+            />
+            <TextInput
+            type='email'
+            id='email'
+            placeholder='email'
+            defaultValue={currentUser.email}
+            //onChange={handleChange}
+            />
+            <TextInput
+            type='password'
+            id='password'
+            placeholder='password'
+            //onChange={handleChange}
+            />
+            <Button
+            type='submit'
+            gradientDuoTone='purpleToPink'
+            outline
+            //disabled={loading || imageFileUploading}
+            >
+            {loading ? 'Loading...' : 'Update'}
+            </Button>
+        </form>
+        <div className='text-red-400 flex justify-between mt-5'>
+            <span className='cursor-pointer'> Delete Account</span>
+            <span className='cursor-pointer'>Sign Out</span>
+        </div>
+    </div>
   )
 }
