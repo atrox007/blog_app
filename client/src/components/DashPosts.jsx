77c36575b0655415b482867b243cdae14458ea0b
atrox007 <ashutosh1kumar007@gmail.com>
2024-12-31 14:12:30 +0530

    added show more posts button

--- client/src/components/DashPosts.jsx
+++ client/src/components/DashPosts.jsx
@@ -37,6 +37,24 @@ export default function DashPosts() {
     }
   }, [currentUser._id]);
 
+  const handleShowMore = async () => {
+    const startIndex = userPosts.length;
+    try {
+      const res = await fetch(
+        `/api/post/getposts?userId=${currentUser._id}&startIndex=${startIndex}`
+      );
+      const data = await res.json();
+      if (res.ok) {
+        setUserPosts((prev) => [...prev, ...data.posts]);
+        if (data.posts.length < 9) {
+          setShowMore(false);
+        }
+      }
+    } catch (error) {
+      console.log(error.message);
+    }
+  };
+
   return (
     <div className='table-auto overflow-x-scroll md:mx-auto p-3 scrollbar scrollbar-track-slate-200 scrollbar-thumb-slate-400 dark:scrollbar-track-slate-700 dark:scrollbar-thumb-slate-600'>
       {currentUser.isAdmin && userPosts.length > 0 ? (
@@ -99,6 +117,13 @@ export default function DashPosts() {
               </Table.Body>
             ))}
         </Table>
+        {showMore && (
+            <button
+              onClick={handleShowMore}
+              className='w-full text-teal-600 self-center text-sm py-7'>
+              Show more
+            </button>
+        )}
         </>
       ):(
         <p>You have not posted anything yet!</p>
