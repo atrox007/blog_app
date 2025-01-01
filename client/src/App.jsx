188ad539388685ffdbbab4f044b165063e2af019
atrox007 <ashutosh1kumar007@gmail.com>
2025-01-01 19:07:57 +0530

    search functionality added

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -14,6 +14,7 @@ import CreatePost from './pages/CreatePost.jsx'
 import UpdatePost from './pages/UpdatePost.jsx'
 import PostPage from './pages/PostPage.jsx'
 import ScrollToTop from './components/ScrollToTop.jsx'
+import Search from './pages/Search.jsx'
 
 export default function App() {
   return (
@@ -26,6 +27,7 @@ export default function App() {
         <Route path="/about" element={<About />}  />
         <Route path="/sign-in" element={<SignIn />}  />
         <Route path="/sign-up" element={<SignUp />}  />
+        <Route path="/search" element={<Search />}  />
         <Route element={<PrivateRoute />}>
           <Route path="/dashboard" element={<Dashboard />}  />
         </Route>
@@ -35,7 +37,6 @@ export default function App() {
         </Route>
         <Route path="/projects" element={<Projects />}  />
         <Route path="/post/:postSlug" element={<PostPage />}  />
-
       </Routes>
       <Footer />
     </BrowserRouter>
