64f43c8f64608e67d35188c631338530cc86e6d0
atrox007 <ashutosh1kumar007@gmail.com>
2025-01-01 22:13:26 +0530

    completed about and projects page

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -1,7 +1,28 @@
 import React from 'react'
 
-export default function about() {
+export default function About() {
   return (
-    <div>about</div>
-  )
+    <div className='min-h-screen flex items-center justify-center'>
+      <div className='max-w-2xl mx-auto p-3 text-center'>
+        <div>
+          <h1 className='text-3xl font font-semibold text-center my-7'>
+            About My Blog
+          </h1>
+          <div className='text-md text-gray-500 flex flex-col gap-6'>
+            <p>
+              Welcome to My Blog! This blog was created by Ashutosh Kumar as a personal project to share his thoughts and ideas with the world. Ashutosh is a passionate developer who loves to code.
+            </p>
+
+            <p>
+              On this blog, you'll find weekly articles and tutorials on topics such as software engineering, programming languages, space n stars, recipes and more. So be sure to check back often for new content!
+            </p>
+
+            <p>
+              We encourage you to leave comments on our posts and engage with other readers. You can like other people's comments and reply to them as well. We believe that a community of learners can help each other grow and improve.
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
